2896c84bf32cd633e12bab54aa24da4dccb5a876
Tankz <157741039+Tankz65@users.noreply.github.com>
2024-06-16 21:21:24 +0200

    Update secure-from-http.lua

--- Security/secure-from-http.lua
+++ Security/secure-from-http.lua
@@ -1,16 +1,9 @@
---// In Development
---// made by tankziwakura
---// Warning: This is for educational purposes only; please do not use it against anyone. Running this, you acknowledge, that you can be banned from Roblox for that.
-
---// Dependencies / Services
 local client = game:GetService("Players").LocalPlayer;
 local HttpService = game:GetService("HttpService");
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local CoreGUI = game:GetService("CoreGui");
 
---// hooking Request
---// see UNC documentation for more information about hookfunction
-hookfunction(getgenv().request, function()
+hookfunction(getgenv().request, function(...)
     print("Request function has been hooked successfully!");
     print("-------------------------------------------------------------");
 end)
